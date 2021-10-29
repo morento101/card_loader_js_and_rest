75b525563a081081a2a5d45114094570e0c664be
morento101 <tlcees@gmail.com>
2021-10-29 09:13:48 +0300

    Utilized functionality for displaying alert when user have seen all the cards

--- static/script.js
+++ static/script.js
@@ -3,6 +3,7 @@ const title = document.getElementById("title");
 const excerpt = document.getElementById("excerpt");
 const profile_img = document.getElementById("profile_img");
 const name = document.getElementById("name");
+const button = document.getElementById('refresh');
 
 const docDate = document.getElementById("date");
 
@@ -14,87 +15,77 @@ const array = [span1, span2, span3];
 const animated_bgs = document.querySelectorAll(".animated-bg");
 const animated_bgs_texts = document.querySelectorAll(".animated-bg-text");
 
+function addAnimatedBg() {
+    animated_bgs.forEach(bg => bg.classList.add('animated-bg'));
+    animated_bgs_texts.forEach(bg => bg.classList.add('animated-bg-text'));
+}
 
-// function fulfillContent(data, count= 0) {
-//
-//     if (count > data.length) {
-//         alert('alert');
-//     }
-//
-//     let img = document.createElement('img');
-//     img.src = data[count].image;
-//     img.alt = "Card's photo";
-//     header.innerHTML = '';
-//     header.appendChild(img);
-//
-//     title.innerHTML = data[count].title;
-//     excerpt.innerHTML = data[count].body;
-//
-//     let img2 = document.createElement('img');
-//     img2.src = data[count].author.image;
-//     img2.alt = "Card's photo";
-//     profile_img.innerHTML = '';
-//     profile_img.appendChild(img2);
-//
-//     name.innerHTML = data[count].author.name;
-//
-//     let date = new Date(data[count].date);
-//     let day = date.getDate();
-//     let month = '0' + date.getMonth();
-//     let year = date.getFullYear();
-//
-//     docDate.innerHTML = '' + day + '.' + month+ '.' + year;
-//
-//     count++
-// }
-
-
-function getData() {
-    $.get("http://127.0.0.1:8000/api/cards/", function(data) {
-        console.log(data);
-
-        // fulfillContent(data);
-
-        let img = document.createElement('img');
-        img.src = data[0].image;
-        img.alt = "Card's photo";
-        header.innerHTML = '';
-        header.appendChild(img);
-
-        title.innerHTML = data[0].title;
-        excerpt.innerHTML = data[0].body;
-
-        let img2 = document.createElement('img');
-        img2.src = data[0].author.image;
-        img2.alt = "Card's photo";
-        profile_img.innerHTML = '';
-        profile_img.appendChild(img2);
-
-        name.innerHTML = data[0].author.name;
-
-        let date = new Date(data[0].date);
-        let day = date.getDate();
-        let month = '0' + date.getMonth();
-        let year = date.getFullYear();
-
-        docDate.innerHTML = '' + day + '.' + month+ '.' + year;
-    });
-
+function removeAnimatedBg() {
     animated_bgs.forEach(bg => bg.classList.remove('animated-bg'));
     animated_bgs_texts.forEach(bg => bg.classList.remove('animated-bg-text'));
 }
 
+let count = 0;
 
+function fulfillContent(data, count) {
+    let jsonData = data.responseJSON;
+    let img = document.createElement('img');
+    img.alt = "Card's photo";
+    header.innerHTML = '';
+    header.appendChild(img);
+    profile_img.innerHTML = '';
+    removeAnimatedBg();
+
+    if (count >= jsonData.length) {
+        img.src = '/media/angry-reactions-with-empty-space-background_79603-1025.jpg';
+        title.innerHTML = 'There are no more cards';
+        excerpt.innerHTML = 'You have watched all cards for today';
+        name.innerHTML = '';
+        docDate.innerHTML = '';
+        button.removeAttribute("onclick");
+    }
+
+    console.log(jsonData);
+    img.src = jsonData[count].image;
+
+    title.innerHTML = jsonData[count].title;
+    excerpt.innerHTML = jsonData[count].body;
+
+    let img2 = document.createElement('img');
+    img2.src = jsonData[count].author.image;
+    img2.alt = "Card's photo";
+    profile_img.appendChild(img2);
+
+    name.innerHTML = jsonData[count].author.name;
+
+    let date = new Date(jsonData[count].date);
+    let day = date.getDate();
+    let month = '0' + date.getMonth();
+    let year = date.getFullYear();
+
+    docDate.innerHTML = '' + day + '.' + month+ '.' + year;
+}
+
+function getData() {
+    return $.get("http://127.0.0.1:8000/api/cards/", function(data) {
+        console.log(data);
+        return data.data
+    });
+}
+
+function container() {
+    fulfillContent(response, count)
+    count++
+}
 
 function newData() {
-    animated_bgs.forEach(bg => bg.classList.add('animated-bg'));
-    animated_bgs_texts.forEach(bg => bg.classList.add('animated-bg-text'));
+    addAnimatedBg();
 
     header.innerHTML = '';
     title.innerHTML = '';
     excerpt.innerHTML = '';
 
-    for (var i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
         array[i].className = 'animated-bg animated-bg-text';
         excerpt.appendChild(array[i])
     }
@@ -103,7 +94,9 @@ function newData() {
     name.innerHTML = '';
     docDate.innerHTML = '';
 
-    setTimeout(getData, 2500);
+    setTimeout(container, 2500);
 }
 
-setTimeout(getData, 2500);
+let response = getData();
+
+setTimeout(container, 2500);
